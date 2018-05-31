47b667c44471e7d75dddb9434ec8a3df024e6315
arizona2014 <andy.lisac@gmail.com>
2018-05-31 09:20:42 +0200

    started creating express function

--- src/server/create-express-app.js
+++ src/server/create-express-app.js
@@ -2,3 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const apiRouter = require('./api-router');
+
+function createExpressApp(database) {
+
+  const app = express();
+
+}
