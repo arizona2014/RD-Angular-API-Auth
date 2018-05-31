bef9ba40d6cd67d24b94eff63f93cc5e4108e9d3
arizona2014 <andy.lisac@gmail.com>
2018-05-31 09:25:10 +0200

    continuing on api function

--- src/server/api-router.js
+++ src/server/api-router.js
@@ -7,4 +7,35 @@ function apiRouter(database) {
 
   const router = express.Router();
 
+  app.get('/api/contacts', (req, res) => {
+
+      // return res.status(500).json({error: 'Error retrieving records'});
+
+      const contactsCollection = database.collection('contacts');
+
+    contactsCollection.find({}).toArray((err, docs) => {
+      return res.json(docs)
+    });
+
+  });
+
+  app.post('/api/contacts', (req, res) => {
+      const user = req.body;
+
+    const contactsCollection = database.collection('contacts');
+
+    contactsCollection.insertOne(user, (err, r) => {
+      if (err) {
+        return res.status(500).json({ error: 'Error inserting new record.' })
+      }
+
+      const newRecord = r.ops[0];
+
+      return res.status(201).json(newRecord);
+
+    });
+
+  });
+
+
 }
