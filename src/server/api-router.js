bc38b10eaa7214195fcb2d38d98fc4bd7f67e73b
arizona2014 <andy.lisac@gmail.com>
2018-05-31 09:23:08 +0200

    started creating api function

--- src/server/api-router.js
+++ src/server/api-router.js
@@ -2,3 +2,9 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const checkJwt = require('express-jwt');
+
+function apiRouter(database) {
+
+  const router = express.Router();
+
+}
