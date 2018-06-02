998a0ad4b4d555f439858f3e5fdd864e2df105d2
arizona2014 <andy.lisac@gmail.com>
2018-06-02 07:23:20 +0200

    cleaning index

--- src/server/index.js
+++ src/server/index.js
@@ -12,33 +12,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/profiles', express.static(path.join(__dirname, 'profiles')));
 app.use(bodyParser.json());
 
-app.get('/api/contacts', (req, res) => {
-
-  // return res.status(500).json({error: 'Error retrieving records'});
-
-  const contactsCollection = database.collection('contacts');
-
-  contactsCollection.find({}).toArray((err, docs) => {
-    return res.json(docs)
-  });
-
-});
-
-app.post('/api/contacts', (req, res) => {
-  const user = req.body;
-
-  const contactsCollection = database.collection('contacts');
-
-  contactsCollection.insertOne(user, (err, r) => {
-    if (err) {
-      return res.status(500).json({ error: 'Error inserting new record.' })
-    }
-
-    const newRecord = r.ops[0];
-
-    return res.status(201).json(newRecord);
-  });
-});
 
 app.get('*', (req, res) => {
   return res.sendFile(path.join(__dirname, 'public/index.html'))
